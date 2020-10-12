f46732f421e77fce3ceaf506f632c20b7c7cfeb7
Darren Ross <darren.ross@bellevuecollege.edu>
2020-10-12 10:38:02 -0700

    Set up project.

---
+++ PalindromeLibrary/CustomQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PalindromeLibrary
+{
+    public class CustomQueue<T>
+    {
+        public int Count { get; set; }
+
+        public void Enqueue<T>(T item)
+        {
+
+        }
+
+        public T Dequeue()
+        {
+
+        }
+
+        public T Peek()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
---
+++ PalindromeLibrary/CustomStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeLibrary
+{
+    class CustomStack<T>
+    {
+        public int Count { get; set; }
+
+        public void push(T item)
+        {
+
+        }
+
+        public T Pop()
+        {
+
+        }
+
+        public T Peek()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
